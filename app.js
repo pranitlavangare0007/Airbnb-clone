a66322fb3aefd43232082711522687d25f4d15b4
Pranit Lavangare <pranitlavangare0007@gmail.com>
2026-02-24 09:40:26 +0530

    added user model

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const listing = require('./routes/listing.js')
 const review = require('./routes/review.js')
 const session =require('express-session')
 const flash =require('connect-flash')
+const passport = require('passport')
+const LocalStratergy=require('passport-local')
+const User =require('./models/user.js')
 
 const port =8080
 const app=express();
@@ -43,6 +46,13 @@ const sessionOption={
 app.use(session(sessionOption))
 app.use(flash())
 
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStratergy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
   
 app.get("/",(req,res)=>{
   console.log(req.cookies)
@@ -54,6 +64,18 @@ app.use((req,res,next)=>{
   res.locals.error= req.flash("error")
   next();
 })
+
+app.get("/demo", async(req,res)=>{
+
+ let fakeUser = new User({
+  email:"pranit@gmail.com",
+  username:"sigma"
+ })
+  let registeredUser = await User.register(fakeUser,"123")
+  res.send(registeredUser)
+
+})
+
 
 app.use("/listing",listing);
 app.use("/listing/:id/review",review);
